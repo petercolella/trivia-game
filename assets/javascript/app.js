0bc590de3fbe945b50c31aa6f38228fd1ec84fae
Krithi <krithis2991@gmail.com>
2019-07-30 21:05:18 -0400

    Timer is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,34 @@
 $(document).ready(function () {
-    $("#timeRemaining").append("<div> 00:00 </div>");
+   // $("#timeRemaining").text("0");
 })
 
+let counter = 0;
+let timeleft = 65;
 
+function timeConverter(t) {
 
-let tensecondsAlert = setTimeout(function() {
-   $("#timeRemaining").append("<div> 00:10 </div>");
-  }, 10000);
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
 
-  let windowTimeOut = setTimeout(function() {
-    $("#timeRemaining").append("<div> 00:15 </div>");
-  }, 15000);
-
-  let wins = 0;
-  let losses = 0;
-
-  function calculatingWins () {
-
-    if($("questionOne").on("click", function() {} ) === inlineRadio1)
-    win++;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
   }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+function countdownTimer() {
+counter++;
+$("#timer").text(timeConverter(timeleft- counter));
+
+} setInterval(countdownTimer, 1000);
+
+
+
