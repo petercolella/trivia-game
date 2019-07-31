61d0e3b32df1953f438c8d9ff2fe3c9f765a9577
Krithi <krithis2991@gmail.com>
2019-07-30 23:33:47 -0400

    Final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,74 @@
-$(document).ready(function () {
-   // $("#timeRemaining").text("0");
-})
+$(document).ready();
 
-let counter = 0;
-let timeleft = 65;
+let counter = 10;
+let timer;
+let currentQuestion = 0;
 
-function timeConverter(t) {
-
-  var minutes = Math.floor(t / 60);
-  var seconds = t - (minutes * 60);
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  if (minutes === 0) {
-    minutes = "00";
-  }
-  else if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  return minutes + ":" + seconds;
+function timeUp() {
+  clearInterval(timer);
 }
 
 function countdownTimer() {
-counter++;
-$("#timer").text(timeConverter(timeleft- counter));
+  counter--;
+  $("#timer").html("<h2>" + counter + "</h2>");
 
-} setInterval(countdownTimer, 1000);
+  if (counter === 0) {
+    timeUp();
+  }
+
+}
+//---------------------------------------------------------------
+
+function nextQuestion() {
+
+  const isQuestionOver = (questionsAnswers.length - 1) === currentQuestion;
+  if (isQuestionOver) {
+    console.log("Game is Over");
+  }
+  else {
+    currentQuestion++;
+    domQuestion();
+  }
+
+};
+
+function domQuestion() {
+
+  counter = 10;
+  timer = setInterval(countdownTimer, 1000);
+
+  const questions = questionsAnswers[currentQuestion].question;
+  const answers = questionsAnswers[currentQuestion].options;
+
+  currentQuestion = $("#triviaGame").html("<h4>" + questions + "</h4>" + getChoice(answers));
+
+}
 
 
+nextQuestion();
+
+function getChoice(answers) {
+  let result = '';
+
+  for (let j = 0; j < answers.length; j++) {
+    result += `<input type="radio" data-answer="${answers[j]}">${answers[j]}`;
+  }
+
+  return result;
+}
+
+$(document).on("click", '.option', function () {
+
+  console.log("Hey");
+});
+
+let correct = 0;
+let incorrect = 0;
+let answered = 0;
+let unanswered = 0;
+
+$("#correct").append(correct);
+$("#incorrect").append(incorrect);
+$("#answered").append(answered);
+$("#unanswered").append(unanswered);
 
---
+++ assets/javascript/triviaQuestions.js
@@ -0,0 +1,33 @@
+const questionsAnswers = [
+    {
+        question: "Where were Ross and Emily supposed to go for their honeymoon?",
+        options: ["Athens", "Paris", "London", "Switzerland"],
+        correctoption: "Athens"
+    },
+    {
+        question: "Which of the actors on 'Friends' unfortunately decides to whiten his teeth before going out on a first date?",
+        options: ["Chandler", "Ross", "Zoey", "Gunther"],
+        correctoption: "Ross"
+    },
+    {
+        question: "Who plays Chandler?",
+        options: ["Matt LeBlanc", "David Schwimmer", "Joshua Jackson", "Matthew Perry"],
+        correctoption: "Matthew Perry"
+    },
+    {
+        question: "Who walked Carol down the aisle when she married Susan?",
+        options: ["Chandler", "Ross", "Zoey", "Gunther"],
+        correctoption: "Ross"
+    },
+    {
+        question: "What was the name that Monica would pick for her son when she had one?",
+        options: ["Ben", "Jim", "Daniel", "Charles"],
+        correctoption: "Daniel"
+    },
+    {
+        question: "What is the name of Ross's son?",
+        options: ["Ben", "Jim", "Daniel", "Charles"],
+        correctoption: "Ben"
+    }
+
+]
