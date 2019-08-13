9f53292e11909cdd9cf0ba74374fe235efcc7c47
petercolella <peter.joseph.colella@gmail.com>
2019-08-13 00:16:40 -0400

    adds showResults() and updates flow of app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,124 @@
-$(document).ready();
+$(document).ready(function() {
+  let counter;
+  let timer;
+  let currentQuestion;
+  let correct;
+  let incorrect;
+  let answered;
+  let unanswered;
+  $('#timer').css('opacity', 0);
 
-let counter = 10;
-let timer;
-let currentQuestion = 0;
+  $('#startButton').on('click', function() {
+    $('#timer').css('opacity', 1);
+    currentQuestion = 0;
+    correct = 0;
+    incorrect = 0;
+    answered = 0;
+    unanswered = 0;
+    nextQuestion();
+  });
 
-function timeUp() {
-  clearInterval(timer);
-}
-
-function countdownTimer() {
-  counter--;
-  $("#timer").html("<h2>" + counter + "</h2>");
-
-  if (counter === 0) {
+  $('#questioButton').on('click', function() {
     timeUp();
+    nextQuestion();
+  });
+
+  function timeUp() {
+    clearInterval(timer);
+    checkAnswer();
   }
 
-}
-//---------------------------------------------------------------
+  function countdownTimer() {
+    counter--;
+    $('#timer').html('<h2>0:0' + counter + '</h2>');
 
-function nextQuestion() {
-
-  const isQuestionOver = (questionsAnswers.length - 1) === currentQuestion;
-  if (isQuestionOver) {
-    console.log("Game is Over");
+    if (counter === 0) {
+      timeUp();
+      nextQuestion();
+    }
   }
-  else {
+  //---------------------------------------------------------------
+
+  function nextQuestion() {
+    $('#startButton').css('opacity', 0);
+    const isQuestionOver = questionsAnswers.length === currentQuestion;
+    if (isQuestionOver) {
+      console.log('Game is Over');
+      showResults();
+    } else {
+      domQuestion();
+    }
+  }
+
+  function domQuestion() {
+    counter = 10;
+    $('#timer').html('<h2>0:' + counter + '</h2>');
+    timer = setInterval(countdownTimer, 1000);
+
+    const questions = questionsAnswers[currentQuestion].question;
+    const answers = questionsAnswers[currentQuestion].options;
+
+    $('#triviaGame').html(`
+<div class="col">
+    <div class="row">
+        <h4>${questions}</h4>
+    </div>
+    <div class="row">
+        <p>${getChoice(answers)}</p>
+    </div>
+</div>
+  `);
+    $('#questioButton').css('opacity', 1);
+  }
+
+  function getChoice(answers) {
+    let result = '';
+
+    for (let j = 0; j < answers.length; j++) {
+      result += `<input type="radio" name="${currentQuestion}" value="${
+        answers[j]
+      }"> ${answers[j]}<br>`;
+    }
+
+    return result;
+  }
+
+  function checkAnswer() {
+    if (
+      $('#triviaGame')
+        .find(`input[name="${currentQuestion}"]:checked`)
+        .val() === questionsAnswers[currentQuestion].correctoption
+    ) {
+      correct++;
+    } else {
+      incorrect++;
+    }
+    if (
+      $('#triviaGame')
+        .find(`input[name="${currentQuestion}"]:checked`)
+        .val()
+    ) {
+      answered++;
+    } else {
+      unanswered++;
+    }
     currentQuestion++;
-    domQuestion();
   }
 
-};
+  function showResults() {
+    $('#startButton')
+      .text('Play Again?')
+      .css('opacity', 1);
 
-function domQuestion() {
+    $('#timer').css('opacity', 0);
 
-  counter = 10;
-  timer = setInterval(countdownTimer, 1000);
-
-  const questions = questionsAnswers[currentQuestion].question;
-  const answers = questionsAnswers[currentQuestion].options;
-
-  currentQuestion = $("#triviaGame").html("<h4>" + questions + "</h4>" + getChoice(answers));
-
-}
-
-
-nextQuestion();
-
-function getChoice(answers) {
-  let result = '';
-
-  for (let j = 0; j < answers.length; j++) {
-    result += `<input type="radio" data-answer="${answers[j]}">${answers[j]}`;
+    $('#triviaGame').html(`
+    <h1>Thanks for playing the game!</h1>
+    <p id="correct"><strong>Correct Answers: ${correct}</strong></p>
+    <p id="incorrect"><strong>Incorrect Answers: ${incorrect}</strong></p>
+    <p id="answered"><strong>Answered Questions: ${answered}</strong></p>
+    <p id="unanswered"><strong>Unanswered Questions: ${unanswered}</strong></p>
+  `);
+    $('#questioButton').css('opacity', 0);
   }
-
-  return result;
-}
-
-$(document).on("click", '.option', function () {
-
-  console.log("Hey");
 });
-
-let correct = 0;
-let incorrect = 0;
-let answered = 0;
-let unanswered = 0;
-
-$("#correct").append(correct);
-$("#incorrect").append(incorrect);
-$("#answered").append(answered);
-$("#unanswered").append(unanswered);
-
